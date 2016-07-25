bd0ed9995f3244c06b1423d05d946b51c27dd697
Viačeslav Pozdniakov <viaceslav.pozdniakov@adform.com>
2016-07-25 23:17:40 +0300

    Handle sig term

--- src/Main.hs
+++ src/Main.hs
@@ -7,14 +7,20 @@ import qualified Baton.Controller as C
 
 import           Control.Applicative
 import           Control.Monad.Reader
+import           Control.Concurrent
+import qualified Control.Exception as E
 import           Data.Monoid        (mconcat)
 import           Data.List.Split    (endBy)
 import           System.Environment
+import           System.Exit
 import           System.Directory
+import           System.Posix.Signals
 import           Web.Scotty.Trans
 
 main :: IO ()
 main = do
+  tid <- myThreadId
+  installHandler sigTERM (Catch (E.throwTo tid ExitSuccess)) Nothing
   marathonUrl <- getEnv "MARATHON_URL"
   registries <- endBy "," <$> getEnv "DOCKER_REGISTRIES"
   args <- getArgs
